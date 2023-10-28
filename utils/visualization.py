eb64ebbc0c9d029947a3b28a2e30e199b554ce0e
Alex <118281736+alexmrin@users.noreply.github.com>
2023-10-28 17:32:56 +0900

    Update visualization.py

--- utils/visualization.py
+++ utils/visualization.py
@@ -1,9 +1,34 @@
 import matplotlib.pyplot as plt
+import torch
+
+voc_mask_colors = torch.tensor([
+    (255, 255, 255),
+    (255, 0, 0),
+    (0, 255, 0),
+    (0, 0, 255),
+    (255, 255, 0),
+    (255, 0, 255),
+    (0, 255, 255),
+    (128, 0, 0),
+    (0, 128, 0),
+    (0, 0, 128),
+    (128, 128, 0),
+    (128, 0, 128),
+    (0, 128, 128),
+    (255, 128, 0),
+    (255, 0, 128),
+    (128, 255, 0),
+    (0, 255, 128),
+    (0, 128, 255),
+    (128, 0, 255),
+    (255, 128, 128),
+    (128, 128, 128)
+], dtype=torch.uint8)
 
 def visualize(pred_mask, groundtruth_mask):
     pred_transformed = voc_mask_transform(pred_mask)
     groundtruth_transformed = voc_mask_transform(groundtruth_mask)
-    
+
     fig, ax = plt.subplots(1, 2, figsize=(10, 5))
     ax[0].imshow(pred_transformed, cmap='hsv')
     ax[0].set_title('Predicted Mask')
@@ -13,5 +38,9 @@ def visualize(pred_mask, groundtruth_mask):
     plt.show()
 
 def voc_mask_transform(mask):
-    transformed_mask = mask * 12.75
-    return transformed_mask.astype(int)
+    transformed_mask = torch.zeros((*mask.shape, 3), dtype=torch.uint8)
+    for idx, color in enumerate(voc_mask_colors):
+        transformed_mask[idx == mask] = color
+    
+    return transformed_mask
+
