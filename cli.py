44585520b7495c22ce3e977e273bed7321a7448f
Alex <118281736+alexmrin@users.noreply.github.com>
2023-10-31 21:01:30 +0900

    Update cli.py

--- cli.py
+++ cli.py
@@ -2,11 +2,26 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    '-c',
-    '--config',
+    '-d',
+    '--dataset',
     type=str,
     required=True,
-    help='Experiment config name.'
+    help='Dataset name.'
+)
+parser.add_argument(
+    '-m',
+    '--model',
+    type=str,
+    required=True,
+    help='Model name.'
+)
+parser.add_argument(
+    '-p',
+    '--path',
+    type=str,
+    required=False,
+    default=None,
+    help='filepath of the state dicts.'
 )
 parser.add_argument(
     '-t',
@@ -16,4 +31,9 @@ parser.add_argument(
     help='Experiment tag name.'
 )
 cli_args = parser.parse_args()
-config, tag = cli_args.config, f'{cli_args.config}--{cli_args.tag}'
+
+
+model_name = cli_args.model
+dataset_name = cli_args.dataset
+dict_path = cli_args.path
+tag = f'{cli_args.model}_{cli_args.dataset}--{cli_args.tag}' if cli_args.dict_path else f'checkpoint_{cli_args.model}_{cli_args.dataset}--{cli_args.tag}' 
