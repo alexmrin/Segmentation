05dbd1f57d8ce2566da6c309a642bb5578f858b7
Alex <118281736+alexmrin@users.noreply.github.com>
2023-11-01 11:19:28 +0900

    Update visualization.py

--- utils/visualization.py
+++ utils/visualization.py
@@ -31,21 +31,25 @@ voc_mask_colors = torch.tensor([
     (128, 128, 128)
 ], dtype=torch.uint8)
 
-def visualize(pred_mask, groundtruth_mask):
+def visualize(image, pred_mask, groundtruth_mask):
+    image = image.numpy().transpose([1, 2, 0])
     pred_transformed = voc_mask_transform(pred_mask).numpy()
     groundtruth_transformed = voc_mask_transform(groundtruth_mask).numpy()
 
-    fig, ax = plt.subplots(1, 3, figsize=(15, 8))
-    ax[0].imshow(pred_transformed)
-    ax[0].set_title('Predicted Mask')
+    fig, ax = plt.subplots(1, 4, figsize=(16, 8))
+    ax[0].imshow(image)
+    ax[0].set_title('Original Image')
+
+    ax[1].imshow(pred_transformed)
+    ax[1].set_title('Predicted Mask')
     
-    ax[1].imshow(groundtruth_transformed)
-    ax[1].set_title('Ground Truth Mask')
+    ax[2].imshow(groundtruth_transformed)
+    ax[2].set_title('Ground Truth Mask')
 
     patches = [mpatches.Patch(color=voc_mask_colors[i].numpy()/255, label=v.mask_dict[i]) for i in range(len(voc_mask_colors))]
-    ax[2].legend(handles=patches, bbox_to_anchor=(0.5, 1), loc='upper center')
-    ax[2].axis('off')
-    ax[2].set_title('Color Map Key')
+    ax[3].legend(handles=patches, bbox_to_anchor=(0.5, 1), loc='upper center')
+    ax[3].axis('off')
+    ax[3].set_title('Color Map Key')
 
     os.makedirs('../comparisons', exist_ok=True)
     plt.savefig(f'../comparisons/comparison_{v.current_epoch}.png')
